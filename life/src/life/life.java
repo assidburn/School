34ba6cb1188cc5fe937da177c12f8c5562bb7f53
Ron Collins <ronshadow9@gmail.com>
2015-11-03 19:33:44 -0800

    hi

--- life/src/life/life.java
+++ life/src/life/life.java
@@ -4,15 +4,16 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
-public class lifepanel {
+public class life {
 
 	boolean[][] cells = new boolean[10][10];
 	JFrame frame = new JFrame("Life simulation");
-	LifePanel = new LifePanel(cells);
+	lifepanel panel = new lifepanel(cells);
 	
 	public life() {
 		frame.setSize(600,600);
 		frame.setLayout(new BorderLayout());
+		frame.add(panel, BorderLayout.CENTER);
 		
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,4 +24,5 @@ public class lifepanel {
 	
 	public static void main(String[] args) {
 		new life();
+}
 }
