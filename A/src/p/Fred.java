f410c1dfa8cf64f5953a9fc41215126d692ef123
Ron Collins <ronshadow9@gmail.com>
2015-11-05 16:29:33 -0800

    haha

--- A/src/p/Fred.java
+++ A/src/p/Fred.java
@@ -2,4 +2,18 @@ package p;
 
 public class Fred {
 
+	Fred() {
+	}
+	
+	public static void main(String[] args) {
+		George g;
+		g = new George();	
+		
+		double a =1.0;
+		double b = 0;
+		double c = 9;
+		g.quadraticRoot(a,b,c);
+		
+	}
 }
+ 
--- A/src/p/George.java
+++ A/src/p/George.java
@@ -2,4 +2,20 @@ package p;
 
 public class George {
 
+	George() { 
+		
+		
+	}
+	
+	double quadraticRoot(double a,double b, double c) {
+		
+		
+		double the = -b + Math.sqrt(16);
+		
+		return the;
+		
+	}
+	
+	
+	
 }
