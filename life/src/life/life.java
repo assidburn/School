33e3373e88add2b2d8e8bb3f8393413277357a24
Ron Collins <ronshadow9@gmail.com>
2015-11-10 09:04:52 -0800

    changes

--- life/src/life/life.java
+++ life/src/life/life.java
@@ -1,9 +1,5 @@
 package life;
 
-import java.awt.BorderLayout;
-
-import javax.swing.JFrame;
-
 public class Life {
 
 	private boolean[][] grid;
@@ -12,7 +8,7 @@ public class Life {
 
 	public static void main(String[] args) {
 		Life x =  new Life(10,10);
-		for(int i = 0; i <5; ++i) {
+		for(int i = 0; i <1; ++i) {
 			x.step();	
 			x.display();
 		}
@@ -20,12 +16,19 @@ public class Life {
 	}
 	
 	private void display() {
-		for(int i = 0; i <maxcolumns; ++i) {
-			for(int j = 0; j <maxrows; ++j) {
-				System.out.println(grid[i][j]);
+		for(int row = 0; row <maxrows; ++row) {	
+			for(int column = 0; column <maxcolumns; ++column)				
+			{
+//				System.out.print(grid[row][column]);
+				
+				if(grid[row][column]) 
+					System.out.print("X");				
+				else
+					System.out.print("O");
 			}
+			System.out.println();
+			
 		}
-		
 		
 	}
 
@@ -33,13 +36,23 @@ public class Life {
 	 * method will compute and display next generation of cells
 	 */
 	private void step() {
+		//for each cell count number of neighbors
+		//0 or 1 neighbors - dies of loneliness
+		//2 or 3 neighbors - survives to next round
+		//4 or more neighbors - dies of overcrowding
+		//If dead 3 neighbors - becomes populated
+		
+
 	}
 
 	public Life (int maxcolumns, int maxrows) {
 		this.maxcolumns = maxcolumns;
 		this.maxrows = maxrows;
 		grid = new boolean [maxcolumns][maxrows];	
-	
+		
+		grid[3][5] = true;
+		grid[3][6] = true;
+		grid[3][7] = true;
 //		System.out.println(maxrows);
 //		System.out.println(maxcolumns);
 		
