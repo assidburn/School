dabcfc3341ce79c00d6847323cecbca280348dd4
Ron Collins <ronshadow9@gmail.com>
2015-12-16 17:35:34 -0800

    updates

--- zuul/src/zuul/Game.java
+++ zuul/src/zuul/Game.java
@@ -39,29 +39,38 @@ class Game
         Room outside, theatre, pub, lab, office;
       
         // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theatre = new Room("in a lecture theatre");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+        //outside = new Room("outside the main entrance of the university");
+        //theatre = new Room("in a lecture theatre");
+        //pub = new Room("in the campus pub");
+        //lab = new Room("in a computing lab");
+        //office = new Room("in the computing admin office");
+        Awing = new Room("in A Wing");
+        Bathroom = new Room (" in boys bathroom");
+        Commons = new Room("In the lower commons");
+        OneHallMainHall = new Room ("In One hall by main hall");
+        MainHall = new Room ("in main hall");
+        Cafeteria = new Room ("in the cafeteria");
+        OneHallGalbraithRoom = new Room ("In 1 hall outside of mr G's room");
+        GalbraithRoom = new Room ("You're a cool kid because you are in Mr G's room");
+        OneHallFHall + new Room ("in one hall by f hall")
         
         // initialise room exits
-        outside.setExit("east", theatre);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
+        //outside.setExit("east", theatre);
+        //outside.setExit("south", lab);
+        //outside.setExit("west", pub);
 
-        theatre.setExit("west", outside);
-        theatre.setExit("south", office);
+        //theatre.setExit("west", outside);
+        //theatre.setExit("south", office);
         
-        pub.setExit("east", outside);
+        //pub.setExit("east", outside);
 
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
+        //lab.setExit("north", outside);
+        //lab.setExit("east", office);
 
-        office.setExit("west", lab);
-        office.setExit("north", theatre);
+        //office.setExit("west", lab);
+        //ffice.setExit("north", theatre);
         
-        currentRoom = outside;  // start game outside
+        //currentRoom = outside;  // start game A Wing
     }
 
     /**
